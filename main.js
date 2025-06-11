7fcb0155c7179332c1a1ece856e87222168da68f
Prof. Gilmar Horchulhak <108031801+GilHorch@users.noreply.github.com>
2025-06-11 11:18:10 -0300

    1

--- main.js
+++ main.js
@@ -19,15 +19,8 @@ const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2023-01-05T00:00:00");
+
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
-
-function atualizaCronometro() {
-    for (let i = 0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
-    }
-}
-
-
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -43,3 +36,15 @@ function calculaTempo(tempoObjetivo) {
 
     return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
+
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+atualizaCronometro();
+setInterval(atualizaCronometro,1000);
+}
+comecaCronometro();
